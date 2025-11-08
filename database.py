22dc4fe916eee1ef69e4f4e9f1c3b98af3337131
Mahesh Mahto <146760262+Maheshmahto@users.noreply.github.com>
2025-11-08 16:02:32 +0530

    Refactor database connection using environment variables
    Updated database connection handling to use environment variables for MySQL configuration.

--- database.py
+++ database.py
@@ -1,20 +1,34 @@
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
+import os 
 from dotenv import load_dotenv
 
 load_dotenv()
+# # db_url = "mysql+pymysql://root:root@localhost:3306/product_data"
+# db_url = os.getenv("db_url")
 
-# Try to get from Railway first, fallback to .env local
-db_url = os.getenv("MYSQL_URL") or os.getenv("db_url")
+# print(db_url, "***************")
 
-# Convert mysql:// → mysql+pymysql:// if needed
-if db_url and db_url.startswith("mysql://"):
-    db_url = db_url.replace("mysql://", "mysql+pymysql://")
+# engine = create_engine(db_url)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
 
-print(db_url, "***************")
+MYSQLHOST = os.getenv("MYSQLHOST")
+MYSQLPORT = os.getenv("MYSQLPORT")
+MYSQLUSER = os.getenv("MYSQLUSER")
+MYSQLPASSWORD = os.getenv("MYSQLPASSWORD")
+MYSQLDATABASE = os.getenv("MYSQLDATABASE")
 
-engine = create_engine(db_url)
+SQLALCHEMY_DATABASE_URL = (
+    f"mysql+pymysql://{MYSQLUSER}:{MYSQLPASSWORD}@{MYSQLHOST}:{MYSQLPORT}/{MYSQLDATABASE}"
+)
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"ssl": {"ssl_disabled": False}},  # Required for Railway
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
