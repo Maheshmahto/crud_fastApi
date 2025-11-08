09f597f4465795032fc2d6bc3e0604c4173c59e3
Mahesh Mahto <146760262+Maheshmahto@users.noreply.github.com>
2025-11-08 15:06:41 +0530

    Refactor database URL retrieval and format handling
    Updated database connection logic to prioritize MYSQL_URL and handle URL format.

--- database.py
+++ database.py
@@ -1,19 +1,22 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os 
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# db_url = "mysql+pymysql://root:root@localhost:3306/product_data"
-db_url = os.getenv("db_url")
+
+# Try to get from Railway first, fallback to .env local
+db_url = os.getenv("MYSQL_URL") or os.getenv("db_url")
+
+# Convert mysql:// → mysql+pymysql:// if needed
+if db_url and db_url.startswith("mysql://"):
+    db_url = db_url.replace("mysql://", "mysql+pymysql://")
 
 print(db_url, "***************")
 
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 # Dependency to get DB session
 def get_db():
